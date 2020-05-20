4c8566a664523b5c288be72e157062b9482a6324
rafael areas <rafa.areas@hotmail.com>
2020-05-20 17:06:18 -0300

    segundo commit

--- 100q.c
+++ 100q.c
@@ -1,3 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+//Considere o seguinte tipo para representar listas ligadas de inteiros//////////////////////////
+
+typedef struct lligada {
+int valor;
+struct lligada *prox;
+} *LInt;
+
+//Questão 1
+
+int length(LInt){
+
+
+}
