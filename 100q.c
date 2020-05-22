95117d471a5097b8c7fdbb47dd88d4f3c9f84403
rafael areas <rafa.areas@hotmail.com>
2020-05-22 02:42:58 -0300

    primeira funçao

--- 100q.c
+++ 100q.c
@@ -9,8 +9,24 @@ struct lligada *prox;
 } *LInt;
 
 //Questão 1
+LInt newLInt (int v, LInt t){
+    LInt new = (LInt) malloc (sizeof (struct lligada));
+    
+    if (new!=NULL) {
+        new->valor = v;
+        new->prox  = t;
+    }
+    return new;
+}
 
-int length(LInt){
+int length(LInt l){
+    if(l==NULL) return 0;
+    else return(1+length(l->prox));
+}
 
+//Questao 2
 
+void freeL(LInt l){
+
+    
 }
